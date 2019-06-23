67638f2f2019b4801a61badf00a248a10f906a0d
OthJ <35712523+OthJ@users.noreply.github.com>
2019-06-23 01:34:06 +0100

    initial commit

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Directive, Input, ViewChild, OnInit, ElementRef} from '@angular/core';
 import { NzCarouselModule } from 'ng-zorro-antd';
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css']
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent implements OnInit{
 
   constructor() { }
 
-  links = ['https://images-na.ssl-images-amazon.com/images/I/71QHGTKiwAL._UX342_.jpg',
-  'https://www.philipwatch.net/i/default/65927/2-montre-philip-watch-caribe-r8253597008.jpg',
-  'https://images-na.ssl-images-amazon.com/images/I/41-DDEn-MPL.jpg'
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  links = ['https://cdn.shopify.com/s/files/1/0377/2037/products/Mens35.Front_bff3a157-e83e-411b-a3e4-609a318e96eb_1024x.jpg?v=1510684666',
+  'https://cdn.shopify.com/s/files/1/0627/5517/products/Chrono-S-Blue_Brown_2048x2048.jpg?v=1542911430',
+  'https://b34959663f5a3997bd0d-2668915a1d3a077262c88fab6aa0aa02.ssl.cf3.rackcdn.com/17170385_1_640.jpg'
 ]
 
 // links = ['1',
@@ -20,6 +23,14 @@ export class SliderComponent implements OnInit {
 // ]
 
   ngOnInit() {
+  }
+
+  
+
+  goNext(){
+    console.log(this.mapElement.nativeElement);
+
+    
   }
 
 }
